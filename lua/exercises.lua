b956b6d712cc9ff652ff50327ca182cb5ef42e56
Jacob Mendoza <112521288+JacNslash04@users.noreply.github.com>
2024-09-11 13:38:05 -0700

    Adding latest working version of problem 1

--- lua/exercises.lua
+++ lua/exercises.lua
@@ -1,5 +1,6 @@
+-- Had to change a few lines to get the program working with my version of Lua 
 function change(amount)
-  if math.type(amount) ~= "integer" then
+  if math.floor(amount) ~= amount then
     error("Amount must be an integer")
   end
   if amount < 0 then
@@ -7,14 +8,24 @@ function change(amount)
   end
   local counts, remaining = {}, amount
   for _, denomination in ipairs({25, 10, 5, 1}) do
-    counts[denomination] = remaining // denomination
+    counts[denomination] = math.floor(remaining / denomination)
     remaining = remaining % denomination
   end
   return counts
 end
 
 -- Write your first then lower case function here
-
+function first_then_lower_case(table_of_strings, predicate)
+  if table_of_strings == {} then
+      return nil
+  end
+  for _, item in ipairs(table_of_strings) do
+    if predicate(item) then
+      return string.lower(item)
+    end
+  end
+  return nil
+end
 -- Write your powers generator here
 
 -- Write your say function here
