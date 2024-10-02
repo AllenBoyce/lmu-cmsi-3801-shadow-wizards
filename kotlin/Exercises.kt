821ca2aaa6a0ef116fcaba489746248947c97e8b
AllenBoyce <allenboyceiii@gmail.com>
2024-10-02 13:37:16 -0700

    kotlin quat v1

--- kotlin/Exercises.kt
+++ kotlin/Exercises.kt
@@ -23,5 +23,30 @@ fun firstThenLowerCase(listOfStrings: List<String>, predicateToCheck: (String) -
 // Write your meaningfulLineCount function here
 
 // Write your Quaternion data class here
+data class Quaternion(val a: Double, val b: Double, val c: Double, val d: Double) {
+    companion object {
+        val ZERO = Quaternion(0.0,0.0,0.0,0.0)
+        val ONE = Quaternion(1.0,0.0,0.0,0.0)
+        val I = Quaternion(0.0,1.0,0.0,0,0)
+        val J = Quaternion(0.0,0.0,1.0,0.0)
+        val K = Quaternion(0.0,0.0,0.0,1.0)
+    }
 
+    operator fun plus(other: Quaternion) {
+        return Quaternion(a + other.a, b + other.b, c + other.c, d + other.d)
+    }
+
+    operator fun times(other: Quaternion) {
+        return Quaternion(
+            a * other.a - b * other.b - c * other.c - d * other.d,
+            a * other.b + b * other.a + c * other.d - d * other.c,
+            a * other.c - b * other.d + c * other.a + d * other.b,
+            a * other.d + b * other.c - c * other.b + d * other.a
+        )
+    }
+
+    fun coefficients(): List<Double> = return listOf(a, b, c, d)
+
+    fun conjugate(): Quaternion = return Quaternion(a, -b, c, -d)
+}
 // Write your Binary Search Tree interface and implementing classes here
