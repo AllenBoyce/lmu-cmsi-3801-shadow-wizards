2127b482c324b0900d6901da0790b697be2cad77
JacNslash04 <JacNslash04@gmail.com>
2024-10-31 18:37:35 -0700

    Adding latest working versions of first_then_apply and meaningful_line_count

--- ocaml/exercises.ml
+++ ocaml/exercises.ml
@@ -13,11 +13,25 @@ let change amount =
     aux amount denominations
 
 (* Write your first then apply function here *)
+let first_then_apply array predicate consumer =
+  match List.find_opt predicate array with
+  | Some x -> consumer x
+  | None -> None
 
 (* Write your powers generator here *)
 
 (* Write your line count function here *)
+let meaningful_line_count file_name =
+  let file = open_in filename in
+  let rec aux count =
+    match input_line file with
+    | exception End_of_file -> count
+    | line -> if String.trim line = "" then aux count else aux (count + 1)
+  in
+  let count = aux 0 in
+  close_in file;
+  count;;
 
-(* Write your shape type and associated functions here *)
+  (* Write your shape type and associated functions here *)
 
 (* Write your binary search tree implementation here *)
